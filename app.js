c4a1c5aac4d0833d79125884672893df4ac23d4a
MoeinDeveloper92 <moeinsamani1992@gmail.com>
2023-03-16 12:18:40 +0330

    adding some functionality

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const btn = document.createElement('button');
 
 const game = {
     score: 0,
-    num: 4
+    num: 2,
+    guesses: 0
 }
 
 output.append(message);
@@ -22,16 +23,19 @@ outputMessage("Click button to start game")
 btn.addEventListener('click', e => {
     if (btn.textContent === "Start Game") {
         game.score = 0
+        game.guesses = 0
         makeBoard();
         btn.textContent = "Check Answer"
         outputMessage("Guess the combo and Adjust the dials")
     } else if (btn.textContent = "Check Answer") {
+        game.guesses++;
         checkAnswer();
     }
 })
 
 
 function makeBoard() {
+    gameArea.innerHTML = ""
     for (let x = 0; x < game.num; x++) {
         const ele = document.createElement('input')
         ele.setAttribute('type', 'number');
@@ -50,11 +54,14 @@ function makeBoard() {
 // below we want to have a function in order to guess the answer
 // we run the funtion if the check function gets cleicked
 function checkAnswer() {
+    let winners = 0;
     const combos = document.querySelectorAll('.dial');
     // we have got a nodeList of all of our input list
     combos.forEach((el) => {
         el.style.color = "white"
+
         if (el.correct == el.value) {
+            winners++;
             el.style.backgroundColor = "green"
         } else if (el.correct > el.value) {
             el.style.backgroundColor = "red"
@@ -62,6 +69,18 @@ function checkAnswer() {
             el.style.backgroundColor = "blue"
         }
     })
+    if (winners === combos.length) {
+        outputMessage("Game Over!!!")
+        gameOver();
+    } else {
+        outputMessage(`You got ${winners} of ${combos.length}-(${game.guesses} guesses)`)
+    }
+}
+
+
+function gameOver() {
+    outputMessage(`Game Over and it took ${game.guesses} Guesses`)
+    btn.textContent = "Start Game"
 }
 
 
